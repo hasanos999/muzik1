672982c80ac8fe1e023a7367a033e3c694efd7ca
Glitch (musicer-bott) <none>
2020-07-02 07:27:57 +0000

    🎆⛄️ Checkpoint ./config/bot.json:6882615/313 ./index.js:6882615/109 ./package.json:6882615/3502 ./commands/devamet.js:6882615/1247 ./commands/durdur.js:6882615/890

--- commands/devamet.js
+++ commands/devamet.js
@@ -3,17 +3,18 @@ const fs = require("fs")
 
 module.exports.run = async (client, message, args) => {
 
-  if(!message.member.voice.channel) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | Sesli kanalda değilsin!` }})
+  if(!message.member.voice.channel) return message.channel.send(`<:voice:720120618275831869> Bir müzik dinleyebilmen için sesli bir kanalda bulunman gerekiyor.`)
     
-  if(!client.player.isPlaying(message.guild.id)) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | Müzik çalmıyor!` }})
+  if(!client.player.isPlaying(message.guild.id)) return message.channel.send(`<:voice:720120618275831869> Şu anda devam ettirebilmem için bir müzik çalmıyor.`)
   
   let song = await client.player.resume(message.guild.id);
-            
-  message.channel.send({embed: {color: client.colors.success, description: `${client.emotes.resume} | Müzik devam ediyor!` }})
+  let çalan = await client.player.nowPlaying(message.guild.id);
+  
+  message.channel.send(`<:invisible:720120617772384269> **${çalan.name}** ${çalan.author} isimli şarkı devam ettiriliyor.`)
     
 }
 
 module.exports.config = {
   name: "devamet",
-  aliases: ['devamet']
+  aliases: ['devam-et']
 }
--- commands/durdur.js
+++ commands/durdur.js
@@ -3,13 +3,13 @@ const fs = require("fs")
 
 module.exports.run = async (client, message, args) => {
 
-  if(!message.member.voice.channel) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | Sesli kanalda değilsin!` }})
+  if(!message.member.voice.channel) return message.channel.send(`<:voice:720120618275831869> Bir müzik durdurmak için sesli bir kanalda bulunman gerekiyor.`)
     
-  if(!client.player.isPlaying(message.guild.id)) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | Müzik çalmıyor!` }})
+  if(!client.player.isPlaying(message.guild.id)) return message.channel.send(`<:voice:720120618275831869> Şu anda durdurabilmem için bir müzik çalmıyor.`)
   
   let song = await client.player.pause(message.guild.id);
             
-  message.channel.send({embed: {color: client.colors.success, description: `${client.emotes.pause} | Müzik durduruldu!` }})
+  message.channel.send(`${client.emotes.stop} Çalan şarkı durduruldu.`)
     
 }
 
